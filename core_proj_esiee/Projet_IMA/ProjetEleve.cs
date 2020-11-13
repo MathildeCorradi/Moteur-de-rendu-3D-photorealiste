04cd3364582414981a2a74eb0506457012c3ede8
antonin pire <antonin.pire@gmail.com>
2020-11-13 12:45:41 +0100

    windoEight -> windowHeight

--- core_proj_esiee/Projet_IMA/ProjetEleve.cs
+++ core_proj_esiee/Projet_IMA/ProjetEleve.cs
@@ -12,27 +12,27 @@ namespace Projet_IMA
     {
         public static void Go()
         {
-            float windowEight = BitmapEcran.GetHeight();
+            float windowHeight = BitmapEcran.GetHeight();
             float windowWidth = BitmapEcran.GetWidth();
 
             List<IShape> objectsScene = new List<IShape>();
-            V3 positionCamera = new V3(windowWidth / 2, -windowWidth, windowEight / 2);
+            V3 positionCamera = new V3(windowWidth / 2, -windowWidth, windowHeight / 2);
             Couleur lightColor = new Couleur(1f, 1f, 1f);
             Lampe lamp = new Lampe(lightColor, new V3(1, -1, 1), 1f);
             lamp.Orientation.Normalize();
 
-            V3 basGauche = new V3(10,400,0);
-            V3 basDroite = new V3(windowWidth-10,400,0);
-            V3 hauteGauche = new V3(10,400, windowEight);
-            V3 hauteDroite = new V3(windowWidth-10, 400, windowEight);
+            V3 basGauche = new V3(10, 400, 0);
+            V3 basDroite = new V3(windowWidth - 10, 400, 0);
+            V3 hauteGauche = new V3(10, 400, windowHeight);
+            V3 hauteDroite = new V3(windowWidth - 10, 400, windowHeight);
 
 
 
             var ground = new Parallelogram(new V3(0, 0, 0), basGauche, new V3(windowWidth, 0, 0), Couleur.GROUND);
-            var ceilling = new Parallelogram(new V3(0, 0, windowEight), new V3(windowWidth, 0, windowEight), hauteGauche, Couleur.CEILLING);
+            var ceilling = new Parallelogram(new V3(0, 0, windowHeight), new V3(windowWidth, 0, windowHeight), hauteGauche, Couleur.CEILLING);
             var wallBack = new Parallelogram(basGauche, hauteGauche, basDroite, Couleur.WALL_BACK);
             var wallRight = new Parallelogram(basDroite, hauteDroite, new V3(windowWidth, 0, 0), Couleur.WALL_RIGHT);
-            var wallLeft = new Parallelogram(new V3(0, 0, 0), new V3(0, 0, windowEight), basGauche, Couleur.WALL_LEFT);
+            var wallLeft = new Parallelogram(new V3(0, 0, 0), new V3(0, 0, windowHeight), basGauche, Couleur.WALL_LEFT);
             var sphr = new Sphere(600, 20, 200, 90, Couleur.SPHERE_BLUE);
             var sphr2 = new Sphere(700, 20, 200, 70, Couleur.SPHERE_LIME);
             var sphr3 = new Sphere(500, 300, 20, 100, Couleur.SPHERE_YELLOW);
@@ -48,7 +48,7 @@ namespace Projet_IMA
 
             for (int xScreen = 0; xScreen <= windowWidth; xScreen++)
             {
-                for (int yScreen = 0; yScreen <= windowEight; yScreen++)
+                for (int yScreen = 0; yScreen <= windowHeight; yScreen++)
                 {
                     V3 positionPixelScene = new V3(xScreen, 0, yScreen);
                     V3 directionRayon = positionPixelScene - positionCamera;
