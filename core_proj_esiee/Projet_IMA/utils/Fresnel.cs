8794c45d5fbb4c54a144818483a9ea575c8cc1fa
BARBERET Remy <remy93130@gmail.com>
2021-05-21 13:37:00 +0200

    Ajout liste indice refraction

---
+++ core_proj_esiee/Projet_IMA/utils/Fresnel.cs
@@ -0,0 +1,15 @@
+﻿namespace Projet_IMA.utils
+{
+    /// <summary>
+    /// Liste d indice de refraction de plusieurs milieux
+    /// <see cref="https://fr.wikipedia.org/wiki/Liste_d%27indices_de_r%C3%A9fraction"/>
+    /// </summary>
+    public static class Fresnel
+    {
+        public const float AIR = 1f;
+        public const float GLASS = 1.66f;
+        public const float WATER = 1.33f;
+        public const float DIAMOND = 2.417f;
+        public const float ICE = 1.309f;
+    }
+}
