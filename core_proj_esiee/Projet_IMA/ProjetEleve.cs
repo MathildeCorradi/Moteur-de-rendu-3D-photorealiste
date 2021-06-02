60bf80df20a7d1a861edaef447417b7868fed541
Mathilde Corradi <mathilde.corradi@gmail.com>
2021-06-02 18:25:51 +0200

    Peaufinement de la scène round 2

--- core_proj_esiee/Projet_IMA/ProjetEleve.cs
+++ core_proj_esiee/Projet_IMA/ProjetEleve.cs
@@ -10,7 +10,7 @@ namespace Projet_IMA
         private static readonly float WindowWidth = Screen.GetWidth();
 
         private static readonly int REFLEXION_NUMBER = 5;
-
+        private static readonly int REFRACTION_NUMBER = 5;
 
         #endregion
 
@@ -31,14 +31,15 @@ namespace Projet_IMA
             //new Sphere(700, 20, 200, 70, new Texture("lead.jpg")),
             //new Sphere(500, 300, 20, 100, MyColor.SPHERE_YELLOW),
             new Sphere(200, 300, 200, 200, new MyColor(0.8f,0.5f,0.5f), null, 0, 1f),
-            new Sphere(800, 300, 200, 50, new MyColor(0.5f,0.5f,0.8f), null, 0, 1f),
-            new Sphere(500, 400, 300, 100, new MyColor(0.5f,0f,0f), new Texture("metal.jpg"), 3f, 0.1f),
+            new Sphere(800, 300, 200, 50, new MyColor(0.5f,0.5f,0.8f), null, 0, .6f),
+            new Sphere(500, 400, 300, 100, new MyColor(1f, .6f, .8f), new Texture("bump4.jpg"), 5f, 0.1f),
+            new Sphere(700, 500, 200, 70, new MyColor(1f, 1f, 1f), null, 0, 0, 0.001f, Fresnel.WATER),
 
             new Parallelogram(new V3(0, -WindowWidth-1, 0), new V3(WindowWidth, -WindowWidth-1, 0), new V3(0, -WindowWidth-1, WindowHeight), new MyColor(.5f,.5f,.5f), true),
             new Parallelogram(new V3(0, -WindowWidth-1, 0), new V3(WindowWidth, -WindowWidth-1, 0), BasGauche, new Texture("tiles4.jpg"), true, new Texture("tiles5.jpg"), 0.001f, 0.3f, 0), //Sol
             new Parallelogram(HautGauche, HautDroite, new V3(0, -WindowWidth-1, WindowHeight), MyColor.CEILLING, true, null, 0, 0.1f), //Plafond
             new Parallelogram(BasGauche, BasDroite, HautGauche, new Texture("luxury2.png"), true, null, 0, 0.06f, 0), //Mur derrière
-            new Parallelogram(BasDroite, new V3(WindowWidth, -WindowWidth-1, 0), HautDroite, MyColor.WALL_RIGHT, true, new Texture("wall2.jpg"), 0.001f, 0.1f, 0), //mur droit
+            new Parallelogram(BasDroite, new V3(WindowWidth, -WindowWidth-1, 0), HautDroite, MyColor.WALL_RIGHT, true, new Texture("stone3.jpg"), 0.002f, 0.1f, 0), //mur droit
             new Parallelogram(new V3(0, -WindowWidth-1, 0), BasGauche, new V3(0, -WindowWidth-1, WindowHeight), MyColor.WALL_LEFT, true, new Texture("wall2.jpg"), 0.001f, 0.1f, 0) //mur gauche
 
             // new Triangle(new V3(1, 1, 1), new V3(WindowWidth, 1, 1), BasGauche, new Texture("carreau.jpg"), true),
@@ -70,7 +71,7 @@ namespace Projet_IMA
                 {
                     V3 currentPixelPosition = new V3(xScreen, 0, yScreen);
                     V3 rayDirection = currentPixelPosition - camera;
-                    MyColor PixelColor = Screen.RayCast(camera, rayDirection, sceneObjects, sceneLights, REFLEXION_NUMBER);
+                    MyColor PixelColor = Screen.RayCast(camera, rayDirection, sceneObjects, sceneLights, REFLEXION_NUMBER, REFRACTION_NUMBER);
                     Screen.DrawPixel(xScreen, yScreen, PixelColor);
                 }
             }
